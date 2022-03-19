d329b83d09c684d0830af35e756834b7b586c344
emersonferreira <emerson.ferreira.net@gmail.com>
2022-03-19 15:00:43 -0300

    Fixing some errors on terraform script

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ terraform {
   backend "azurerm" {
         resource_group_name  = "servian"
         storage_account_name = "strservian"
-        container_name       = "tfstate"
+        container_name       = "servian-tfstate"
         key                  = "servian-terraform.tfstate"
     }
 }
